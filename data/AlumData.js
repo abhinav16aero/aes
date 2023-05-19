a9cb5793f51379726b8fddb673479700b108566c
Abhinav <114936870+abhinav16aero@users.noreply.github.com>
2023-05-19 20:27:51 +0530

    Update AlumData.js

--- data/AlumData.js
+++ data/AlumData.js
@@ -1,1 +1,16 @@
+const projectsData = [
+  {
+    title: 'Sample 1',
+    description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`,
+    imgSrc: '/static/images/google.png',
+    href: 'https://aeschatify.vercel.app',
+  },
+  {
+    title: 'Sample 2',
+    description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`,
+    imgSrc: '/static/images/time-machine.jpg',
+    href: 'https://www.google.com',
+  },
+]
 
+export default projectsData
