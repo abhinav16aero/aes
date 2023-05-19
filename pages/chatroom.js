d6d191286c215ef12691aaef091dd0301d996e74
Abhinav <114936870+abhinav16aero@users.noreply.github.com>
2023-05-19 20:16:12 +0530

    Update chatroom.js

--- pages/chatroom.js
+++ pages/chatroom.js
@@ -1,6 +1,35 @@
-import { getAllFilesFrontMatter } from '@/lib/mdx'
 import siteMetadata from '@/data/siteMetadata'
-import ListLayout from '@/layouts/ListLayout'
+import projectsData from '@/data/AlumData'
+import Card from '@/components/Card'
 import { PageSEO } from '@/components/SEO'
 
-
+export default function Chat with Alumni() {
+  return (
+    <>
+      <PageSEO title={` Alumni - ${siteMetadata.author}`} description={siteMetadata.description} />
+      <div className="divide-y divide-gray-200 dark:divide-gray-700">
+        <div className="space-y-2 pt-6 pb-8 md:space-y-5">
+          <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
+            Alumni
+          </h1>
+          <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
+            Alumni.
+          </p>
+        </div>
+        <div className="container py-12">
+          <div className="-m-4 flex flex-wrap">
+            {projectsData.map((d) => (
+              <Card
+                key={d.title}
+                title={d.title}
+                description={d.description}
+                imgSrc={d.imgSrc}
+                href={d.href}
+              />
+            ))}
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
