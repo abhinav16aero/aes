cd974d346cf4ea3e92f84493178c826066edef2a
Abhinav <114936870+abhinav16aero@users.noreply.github.com>
2023-05-19 20:07:45 +0530

    Update headerNavLinks.js

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -1,9 +1,10 @@
 const headerNavLinks = [
-  { href: '/', title: 'Home' },
+//   { href: '/', title: 'Home' },
   { href: '/blog', title: 'Blogs' },
   { href: '/tags', title: 'Find' },
   { href: '/projects', title: 'Events' },
   { href: '/about', title: 'About' },
+  { href: '/chatroom', title: 'Chat with Alumni' },
 ]
 
 export default headerNavLinks
